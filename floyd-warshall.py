6fd9e8354015c9f9b4582d135389a308a0e111de
Thaha Muhammed <mthaha1989@gmail.com>
2016-09-27 16:07:08 +0300

    modified code to print the path also

--- floyd-warshall.py
+++ floyd-warshall.py
@@ -1,10 +1,28 @@
 import sys
 
+
+
+def pathshortest(path,pathMatrix,i,j):
+		
+	if i == j:
+		path.append(i)	
+	elif pathMatrix[i][j] is None:
+		print "No path exists"       		
+		return []
+   	
+	else:
+   		pathshortest(path,pathMatrix, i, pathMatrix[i][j])
+       		path.append(j)
+	return path
+	
+
 graphFile = open("grafo.txt")
 
-size = graphFile.readline()
-size = int(size)
+size = graphFile.readline() 						
+size = int(size) 							
 graphMatrix = [[]] * size
+pathMatrix = [[0 for x in range(size)] for x in range(size)] 
+						
 
 for i in range(size):
 	graphMatrix[i] = graphFile.readline()
@@ -13,23 +31,45 @@ for i in range(size):
 
 graphFile.close()
 
-for k in range(size):
-	for i in range(size):
-		if graphMatrix[k][i] == 'n':
-			graphMatrix[k][i] = sys.maxint
+for i in range(size):
+	for j in range(size):
+		if graphMatrix[i][j] == 'n':
+			graphMatrix[i][j] = sys.maxint
+			pathMatrix[i][j] = None
 		else:
-			graphMatrix[k][i] = int(graphMatrix[k][i])
+			graphMatrix[i][j] = int(graphMatrix[i][j])
+			pathMatrix[i][j] = i
 
 resultMatrix = graphMatrix
+		
+
 
 for k in range(size):
 	for i in range(size):
 		for j in range(size):
-			resultMatrix[i][j] = min(resultMatrix[i][j], resultMatrix[i][k] + resultMatrix[k][j])
+			if (resultMatrix[i][j] > resultMatrix[i][k] + resultMatrix[k][j]):
+				resultMatrix[i][j] = resultMatrix[i][k] + resultMatrix[k][j]
+				pathMatrix[i][j] = pathMatrix[k][j]
 
 resultFile = open("resultado.txt", 'w')
+
 
 for i in range(size):
 	resultFile.write(str(resultMatrix[i]).strip('[]') + '\n')
 
 resultFile.close()
+
+while True:
+	u = raw_input("\n\nEnter Soursce Node:")
+	v = raw_input("\n\nEnter Destination Node:")
+	print "The shortest path is through \n"
+	path = []
+	pathfinal = pathshortest(path,pathMatrix,int(u)-1,int(v)-1)
+	print map(lambda x:x+1, pathfinal)
+	print "The shortest distance is \n"
+	print resultMatrix[int(u)-1][int(v)-1]
+	exit = raw_input("\n\nDo you want to exit[Y or y]")
+	if exit == 'Y' or exit == 'y':
+		break
+
+	
